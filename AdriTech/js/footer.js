de802b16092dae62ba975760d9e9d31c9c480b0d
adritech99 <adritech999@gmail.com>
2025-12-30 22:31:16 +0100

    Add files via upload

---
+++ AdriTech/js/footer.js
@@ -0,0 +1,10 @@
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer-container").innerHTML = data;
+
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
+  });

19fb75a911f0892ab3c1475aa4c229ffb18a9c2a
AdriTech <adritech999@gmail.com>
2026-01-03 22:24:40 +0100

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-fetch("../header.html")
+fetch("header.html")
   .then(function (response) {
     return response.text();
   })
@@ -34,60 +34,34 @@ function ajustarPosicionTexto() {
 
 window.addEventListener('resize', ajustarPosicionTexto);
 
-
-
 // reloj...
-
 function iniciarReloj() {
 
   var contenedor = document.getElementById("fecha-hora");
 
-
-
   function actualizarFechaHora() {
-
     var ahora = new Date();
-
     var opcionesFecha = {
-
       weekday: "long",
-
       year: "numeric",
-
       month: "long",
-
       day: "numeric"
-
     };
 
-
-
     var fecha = ahora.toLocaleDateString("es-ES", opcionesFecha);
-
-
 
     fecha = fecha.split(" ").map(function(palabra) {
 
       if (palabra.toLowerCase() === "de") return "de";
-
       return palabra.charAt(0).toUpperCase() + palabra.slice(1);
-
     }).join(" ");
 
-
-
     var hora = ahora.toLocaleTimeString("es-ES");
-
     contenedor.textContent = fecha + " | " + hora;
-
   }
 
-
-
   actualizarFechaHora();
-
   setInterval(actualizarFechaHora, 1000);
-
 }
 
 
@@ -95,53 +69,30 @@ function iniciarReloj() {
 function marcarMenuActivo() {
 
   const enlaces = document.querySelectorAll('.menu-navegacion a');
-
   // Obtenemos el nombre del archivo actual (ej: proyectos.html o pcb_adc.html)
-
   let rutaActual = window.location.pathname.split('/').pop() || 'index.html';
 
-
-
   enlaces.forEach(enlace => {
-
     // Limpiamos clases previas
-
     enlace.classList.remove('active');
-
-
 
     const href = enlace.getAttribute('href').split('/').pop();
 
-
-
     if (rutaActual === href) {
-
       // 1. Marcamos el enlace donde estamos
-
       enlace.classList.add('active');
 
-
-
       // 2. BUSCAMOS AL PADRE: Si este enlace está dentro de un submenú...
-
       const submenu = enlace.closest('.submenu');
-
       if (submenu) {
-
         // Buscamos el enlace principal que está justo antes del submenú
-
         const padrePrincipal = submenu.parentElement.querySelector('a');
-
         if (padrePrincipal) {
 
-          padrePrincipal.classList.add('active');
-
+          padrePrincipal.classList.add('active')
         }
-
       }
-
     }
-
   });
 
 }
